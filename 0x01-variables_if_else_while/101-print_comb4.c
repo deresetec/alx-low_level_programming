8c589f221bfa62417962a7eb1b08701b28a245b0
deresetec <deresetec@gmail.com>
2022-12-12 09:49:24 -0800

    Combination4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,7 +12,8 @@ int main(void)
 
 for (digit1 = 0; digit1 < 8; digit1++)
 {
-for (digit2 = digit1 + 1; digit2 < 9; digit2++)							
+for (digit2 = digit1 + 1; digit2 < 9; digit2++)
+
 {
 for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 {
@@ -22,9 +23,9 @@ putchar((digit3 % 10) + '0');
 if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 continue;
 putchar(',');
-putchar(' ');
-												
+putchar(' ');												
 }
+
 }
 }
 putchar('\n');
