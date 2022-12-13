81a12415af529074a10ee5a7d8e3cadf88ceba16
deresetec <deresetec@gmail.com>
2022-12-13 12:01:37 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,18 @@
+#include "main.h"
 
+/**
+ * main - prints _putcar
+ * Return: 0
+ */
+int main(void)
+{
+       char c[] = "_putcar";
+       int i;
+
+for (i = 0; i< 9; i++)
+{
+       _putchar(c[i]);
+}	
+       _putchar('\n');
+       return (0);
+}
