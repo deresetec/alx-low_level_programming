115026dbdf7758e0fd4bbdcc1266eb9023dca57d
Derese <deresetec@gmail.com>
2023-03-20 04:36:06 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/** 
+ * main - main entry point
+ *
+ * print lowercase alphabet 
+ */
+int main(void)
+{
+char ch = 'a';
+
+while(ch <= 'z')
+putchar(ch++);
+putchar('\n');
+return (0);
+}
